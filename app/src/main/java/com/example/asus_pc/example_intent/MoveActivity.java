65c161256e916fcba8dd7184a04bb24510ebbc42
ekachristian <33279246+ekachristian@users.noreply.github.com>
2018-09-04 11:24:30 +0700

    Intent

---
+++ app/src/main/java/com/example/asus_pc/example_intent/MoveActivity.java
@@ -0,0 +1,13 @@
+package com.example.asus_pc.example_intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+}
